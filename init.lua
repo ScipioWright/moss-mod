c72d1b135d0788744aa9b86d35c214d1f9587046
Scipio Wright <scipiowright@gmail.com>
2023-04-28 11:40:08 -0400

    Placeholder stuff for extras
    For when extras get added in, will be a toggleable option

--- init.lua
+++ init.lua
@@ -1,1 +1,27 @@
-print("moss has loaded successfully")
+dofile_once("data/scripts/lib/utilities.lua")
+dofile_once("data/scripts/perks/perk.lua")
+
+
+local function get_player()
+	return EntityGetWithTag("player_unit")[1]
+end
+
+
+function give_perk(perk_name)
+    local player = get_player()
+	local x, y = EntityGetTransform(player)
+	local perk = perk_spawn(x, y, perk_name)
+	perk_pickup(perk, p, EntityGetName(perk), false, false)
+end
+
+
+function give_extras()
+    print("tbd")
+end
+
+
+function OnPlayerSpawned(player_id)
+    if ModSettingsGet("extras") == true then
+        give_extras()
+    end
+end
