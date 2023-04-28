d09b4ee4f2834af081e18bee7c198d3a05a1e55f
Scipio Wright <scipiowright@gmail.com>
2023-04-28 13:58:00 -0400

    Added comments
    also fixed an incorrect variable name

--- init.lua
+++ init.lua
@@ -1,26 +1,26 @@
-dofile_once("data/scripts/lib/utilities.lua")
-dofile_once("data/scripts/perks/perk.lua")
+dofile_once("data/scripts/lib/utilities.lua") -- Noita's internal utilities
+dofile_once("data/scripts/perks/perk.lua") -- Perk-related commands
 
 
-local function get_player()
+local function get_player() -- outputs the player's entity ID
 	return EntityGetWithTag("player_unit")[1]
 end
 
 
 function give_perk(perk_name)
-    local player = get_player()
-	local x, y = EntityGetTransform(player)
+	local player_id = get_player()
+	local x, y = EntityGetTransform(player_id)
 	local perk = perk_spawn(x, y, perk_name)
-	perk_pickup(perk, p, EntityGetName(perk), false, false)
+	perk_pickup(perk, player_id, EntityGetName(perk), false, false)
 end
 
 
-function give_extras()
-    give_perk("BLEED_SLIME")
+function give_extras() -- todo: something to check whether the player has the extras already
+    give_perk("BLEED_SLIME") -- Slime Blood perk
 end
 
 
-function OnPlayerSpawned(player_id)
+function OnPlayerSpawned(player_id) -- called when the player spawns on both new game and continue
     if ModSettingsGet("extras") == true then
         give_extras()
     end
